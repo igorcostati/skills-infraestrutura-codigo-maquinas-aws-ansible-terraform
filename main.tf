4cf38076742986ff631492074f9e34571a448424
Igor Costa <igor.costa@tecnologia-i.com.br>
2024-10-16 20:13:50 -0300

    4 terraform, ansible e shell

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ resource "aws_instance" "app_server" {
                 nohup busybox httpd -f -p 8080 &
               EOF */
   tags = {
-    Name = "primeira-instancia-1"
+    Name = "terraforma-ansible-python"
   }
 }
