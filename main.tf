261c0b82ae0eeaa74526d9bcb981925694293bc7
Igor Costa <igor.costa@tecnologia-i.com.br>
2024-10-16 18:38:52 -0300

    Modificações 3-terraform e ansible

--- main.tf
+++ main.tf
@@ -19,13 +19,13 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "labs-ec2"
   vpc_security_group_ids = ["sg-0f5934cb1eee431df","sg-0cd2567f755a6f5a4","sg-042cf664745e934ca"]
-  user_data = <<-EOF
+  /* user_data = <<-EOF
               #!/bin/bash
-              cd /home/ubuntu
-              echo "<h1>Feito com Terraform</h1>" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+                cd /home/ubuntu
+                echo "<h1>Feito com Terraform 2</h1>" > index.html
+                nohup busybox httpd -f -p 8080 &
+              EOF */
   tags = {
-    Name = "primeira-instancia"
+    Name = "primeira-instancia-1"
   }
 }
